a0754fde0c8ddad9a9d2f2415a9bc212626fe0cc
Robert Nelson <robertcnelson@gmail.com>
2021-10-14 16:58:39 -0500

    what broke
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 sudo rm -f /boot/firmware/tiboot3.bin ;\
 sudo rm -f /boot/firmware/tispl.bin ;\
 sudo rm -f /boot/firmware/u-boot.img ;\
-sudo rm -f /boot/firmware/sysfw.itb ;\
 sync
 
 sudo cp -v ./tiboot3.bin /boot/firmware/ ;\
 sudo cp -v ./tispl.bin /boot/firmware/ ;\
 sudo cp -v ./u-boot.img /boot/firmware/ ;\
-sudo cp -v ./sysfw.itb /boot/firmware/ ;\
 sync
 
+#sudo rm -f /boot/firmware/sysfw.itb ;\
+#sudo cp -v ./sysfw.itb /boot/firmware/ ;\
