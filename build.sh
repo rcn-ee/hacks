9c271b7c7fb7d3dc78f6e0436885be930f9c91d3
Robert Nelson <robertcnelson@gmail.com>
2021-10-14 16:17:41 -0500

    use ti branch of arm-trusted-firmare
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ make SOC=j721e CROSS_COMPILE=arm-linux-gnueabihf-
 mv sysfw.itb ../
 cd ../
 
-git clone https://github.com/beagleboard/arm-trusted-firmware
+git clone -b 08.00.00.004 https://github.com/beagleboard/arm-trusted-firmware
 cd ./arm-trusted-firmware/
-make CROSS_COMPILE=aarch64-linux-gnu- ARCH=aarch64 PLAT=k3 TARGET_BOARD=generic SPD=opteed
+make -j4 CROSS_COMPILE=aarch64-linux-gnu- ARCH=aarch64 PLAT=k3 TARGET_BOARD=generic SPD=opteed
 mv build/k3/generic/release/bl31.bin ../
 cd ../
 
 git clone https://github.com/beagleboard/optee_os
 cd ./optee_os/
-make PLATFORM=k3-j721e CFG_ARM64_core=y
+make -j4 PLATFORM=k3-j721e CFG_ARM64_core=y
 mv out/arm-plat-k3/core/tee-pager_v2.bin ../
 cd ../
 
